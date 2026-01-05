d610effbb10e7821796e5a7d4fecb3c523c9d0f6
Wisi <rewisi180502@gmail.com>
2026-01-04 22:00:19 -0600

    Add Periodo model and migrations

--- catalogos/models.py
+++ catalogos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db import transaction
 
-# Create your models here.
+class Periodo(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)  # ej. "2026-1"
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    activo = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-fecha_inicio"]
+
+    def __str__(self):
+        estado = "Activo" if self.activo else "Inactivo"
+        return f"{self.nombre} ({estado})"
+
+    def save(self, *args, **kwargs):
+        with transaction.atomic():
+            if self.activo:
+                Periodo.objects.exclude(pk=self.pk).update(activo=False)
+            super().save(*args, **kwargs)
